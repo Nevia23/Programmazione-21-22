520ed46eac333d005f23db6cbf38f2bd26b4a59c
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-03-08 19:06:17 +0100

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * Stampare l'insieme dei divisori non banali 
+ * di un numero x>0. Un divisore non banale è 
+ * un divisore diverso da 1 e x. 
+ * Inoltre, se x è primo, stampare 
+ * "il numero x è primo".
+ * 
+ * Es. se x=5 stampa
+ * Il numero 5 è primo
+ * 
+ * Es. se x=6 stampa
+ * Il numero 6 è divisibile per 2
+ * Il numero 6 è divisibile per 3
+ * 
+ * Per la consegna utilizzare x=63
+ * 
+ */
+
+int main() {
+
+
+}
