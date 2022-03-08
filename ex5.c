96fd56dc5cac4b29a08aea6964309c8e363e9d39
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-03-08 19:07:59 +0100

    Create ex5.c

---
+++ ex5.c
@@ -0,0 +1,16 @@
+/*
+ * Stampare una piramide rovesciata utilizzando il carattere #,  
+ * in modo che la punta della piramide sia in basso e la base in alto. 
+ * Per esempio se ALTEZZA=3 si ottiene il seguente output: 
+ * 
+ * #####
+ *  ###
+ *   #
+ * 
+ * Per la consegna utilizzare ALTEZZA=6
+ * 
+ */
+
+int main (void) {
+
+}
