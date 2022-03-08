08b2c309cff0b9561ab6547b59042a14882008bd
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-03-08 19:07:00 +0100

    Create ex3.c

---
+++ ex3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * Scrivete un programma che definisca una costante ALTEZZA=x e disegni un triangolo rettangolo di base ed 
+ * altezza x, utilizzando il carattere #. 
+ * In particolare il programma stamperà # una volta per la prima riga, # due volte per la seconda riga, 
+ * fino a stampare # x volte per la x-esima riga. 
+ *
+ * Per esempio se ALTEZZA=3 si ottiene il seguente output: 
+ * #
+ * ##
+ * ###
+ * 
+ * Per la consegna utilizzare ALTEZZA=13
+ */
+ 
+
+int main () {
+
+}
