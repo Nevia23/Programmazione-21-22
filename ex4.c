cce3dbf8ae9a95c28bd95652c9c00e7c700c15a2
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-03-08 19:07:33 +0100

    Create ex4.c

---
+++ ex4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+ * Data la costante LIMIT, stampare la serie di 
+ * fibonacci, fib(k), a partire da fib(0) fino al
+ * valore fib(k) più grande t.c. fib(k) < LIMIT.
+ * 
+ * La funzione di Fibonacci è definita per casi:
+ * 
+ * fib(0) = 0
+ * fib(1) = 1
+ * fib(i) = fib(i-1) + fib(i-2)
+ * 
+ * Es. se LIMIT = 25 stampa
+ * 0
+ * 1
+ * 1
+ * 2
+ * 3
+ * 5
+ * 8
+ * 13
+ * 21
+ * 
+ * Per la consegna assegnare LIMIT=56
+ * 
+ */
+
+int main() {
+
+}
+
