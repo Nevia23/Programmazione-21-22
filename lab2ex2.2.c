de171ea489ba2d4161c8aedbb5e58618cb0199b3
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-03-13 19:19:41 +0100

    Create lab2ex2.2.c

---
+++ lab2ex2.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+* Scrivete un programma che trovi tutti i numeri perfetti tra due estremi. Sia estremi che i numeri trovati dovranno essere interi.
+* Un numero perfetto si definisce tale quando la somma di tutti i suoi divisori eccetto se stesso è uguale al sumero stesso.
+*
+* Il programma deve stampare i valori trovati, utilizzare
+* printf("Il numero %d è perfetto\n", limite_inferiore);
+*
+* Per la consegna utilizzare il seguenti limiti:
+* limite_inferiore = 4
+* limite_superiore = 30
+*/
+
+int main (void) {
+
+}
