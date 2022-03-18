a144c12cf8cd77320e6289b222ca779d10a359f8
Nevia23 <bzaghetto23@gmail.com>
2022-03-18 17:05:47 +0100

    ok

--- lab2ex2.1.c
+++ lab2ex2.1.c
@@ -13,4 +13,21 @@
 
 int main (void) {
 
+    int L[] = {3, 5, 1, 7, 0, 9, 4, 6, 2, 8};
+    int min_indice = 0, max_indice = 0, i;
+
+    for (i = 1; i < 10; i++) {
+
+        if (L[i] < L[min_indice]) {
+            min_indice = i;
+        }
+
+        if (L[i] > L[max_indice]) {
+            max_indice = i;
+        }
+    }
+
+    printf("Valore minimo: array[%d] -> %d\n", min_indice, L[min_indice]);
+    printf("valore massimo: array[%d] -> %d", max_indice, L[max_indice]);
+
 }
