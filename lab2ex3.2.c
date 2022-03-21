03ee6b051a2a177fc7619d5390be3da326b62ce5
Nevia23 <bzaghetto23@gmail.com>
2022-03-21 17:57:21 +0100

    Update lab2ex3.2.c

--- lab2ex3.2.c
+++ lab2ex3.2.c
@@ -13,4 +13,26 @@
 
 int main (void) {
 
+    int array[] = {3, 5, 7, 1, 2, 9, 4, 6, 0, 8};
+    int saved, min_indice, i, j;
+
+    for (i = 0; i < 10; i++) {
+
+        min_indice = i;
+        saved = array[i];
+
+        for (j = i; j < 10; j++) {
+
+            if (array[j] < array[min_indice]) {
+                min_indice = j;
+            }
+        }
+
+        array[i] = array[min_indice];
+        array[min_indice] = saved;
+    }
+
+    for (i = 0; i < 10; i++) {
+        printf("Array[%d] : %d\n", i, array[i]);
+    }
 }
