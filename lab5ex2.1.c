c54b5360cd88283377ef70d13a9d70a60833a804
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-04-08 14:55:22 +0200

    Create lab5ex2.1.c

---
+++ lab5ex2.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* 
+* Scrivere un programma che dato in input una stringa, verifichi con una funzione ricorsiva se è palindroma.
+* Il programma deve poi stampare quanto trovato, utilizzare:
+* if ( stringa_palindroma == 1)
+*     printf("%s palindroma\n",S);
+* else
+*     printf("%s non palindroma\n",S);
+*/
+
+
+int main(void) {
+    int len;
+    scanf("%d",&len);
+    char S[len];
+    scanf("%s",S);
+    
+    
+    return 0;
+}
