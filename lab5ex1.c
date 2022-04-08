b220b67271f804499cf10d34e9b56fc2e6d1f7f7
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-04-08 14:54:59 +0200

    Create lab5ex1.c

---
+++ lab5ex1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+ * Scrivere una funzione ricorsiva che calcoli la 
+ * lunghezza di una stringa. 
+ * Scegliete voi il nome ed il prototipo della 
+ * funzione. 
+ * Nel main leggete da tastiera la stringa di cui
+ * calcolar la lunghezza. 
+ * Ad esempio se la stringa in input è "ciao", viene
+ * stampato:
+   4
+
+ */
+
+
+int main(void) {
+
+    char s[100]; //lunghezza massima stringa in input
+    scanf("%s", s);
+    int lunghezza_s;
+
+    /* 
+     * Inserite qua la chiamata alla vostra funzione 
+     * che calcola la lunghezza della stringa salvando
+     * il valore restituito in lunghezza_s. 
+     */
+    //lunghezza_s = 
+    printf("%d\n", lunghezza_s);
+
+}
