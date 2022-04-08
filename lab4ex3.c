872e2f17d9216109da17cf7c584b1356dc323818
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-04-08 14:53:01 +0200

    Create lab4ex3.c

---
+++ lab4ex3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * Scrivete un programma che dato un array di interi positivi, calcoli quante 
+ * sono le occorrenze di ogni valore presente nell'array.
+ * Notate che i valori dell'array, purché positivi, 
+ * possono essere grandi a piacere. 
+ * Il programma deve poi stampare quanto trovato, utilizzando il comando:
+ * printf("il valore %d appare %d volte\n", A[i], freq[i]);
+ * 
+ * Per esempio se l'array è [1,1,2,3,1,2] si ottiene il seguente output:
+ * il valore 1 appare 3 volte
+ * il valore 2 appare 2 volte
+ * il valore 3 appare 1 volte
+ * 
+ * Per la consegna usare l'array [2,5,6,2,7,6,6,7,7,7]
+ * 
+ */
+
+int main () {
+
+}
