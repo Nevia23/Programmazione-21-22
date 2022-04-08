fc995250c419ab4ebbf9cbf98d7e78a548ffc558
Nevia23 <bzaghetto23@gmail.com>
2022-04-08 16:22:38 +0200

    Update lab3ex2.3.c

--- lab3ex2.3.c
+++ lab3ex2.3.c
@@ -8,21 +8,49 @@
  * ES: N="programmazione2020", T="ez" -> "No match found"
 */
 
-unsigned int len(char* N);
+unsigned int len (char* N);
 
 int main(){
 
     /* Nota:
      * 256 è la dimensione massima della stringa ma il numero di caratteri può essere inferiore
      * implementare una funzione len per misurare la lunghezza di una stringa
-     * (contare quanti caratteri ci sono prima del delimiter "\0")
+     * (contare quanti caratteri ci sono prima del delimiter "\0")*/
+
     char N[256];
     char T[256];
 
     scanf("%255s", N);
     scanf("%255s", T);
 
-    int N_len =len(N), T_len =len(T);
+    int N_len = len(N), T_len = len(T);
+    int i, j = 0;
 
-    //inserire codice qui
+    for (i = 0; i < N_len && j < T_len; i += 1) {
+
+        if (N[i] == T[j]) {
+            j += 1;
+        } else {
+            i = i - j;
+            j = 0;
+        }
+    }
+
+    if (j == T_len) {
+        int match = i - T_len;
+        printf("Match found at position %d\n", match);
+    } else {
+        printf("No match found\n");
+    }
+}
+
+unsigned int len (char* N) {
+
+    unsigned int contatore = 0;
+
+    while (*(N + contatore) != '\0') {
+        contatore += 1;
+    }
+
+    return contatore;
 }
