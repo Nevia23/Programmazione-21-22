b11f3d5d5407c57ef7bff6a6537c0b2901b2921c
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-04-08 14:51:54 +0200

    Create lab4ex1.c

---
+++ lab4ex1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+* Scrivete un programma che calcoli la trasposta di una matrice generica.
+*
+* L'operazione consiste nello scambiare le righe con le colonne.
+*
+* Esempio:
+* |a b c| ____ |a d|
+* |d e f|      |b e|
+*              |c f|
+*
+* Per la consegna, utilizzare:
+* matrice = [[1,2],[3,4],[5,6]]
+*
+* Utilizzare il seguente codice per stampare il risultato:
+* for (int i = 0; i < colonne; i += 1){
+*   for (int j = 0; j < righe; j += 1){
+*       printf("%d ", matRisultato[i][j]); 
+*   }
+*   printf("\n");
+* }
+* 
+*/
+
+int main (void) {
+
+}
