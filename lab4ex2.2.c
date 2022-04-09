4f47aea76a43f84cb743364a016a07f31d3be6ed
Nevia23 <bzaghetto23@gmail.com>
2022-04-09 22:09:20 +0200

    Update lab4ex2.2.c

--- lab4ex2.2.c
+++ lab4ex2.2.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+/*
+Data una matrice di dimensioni arbitrarie, definire una funzione calcola_somma che salva in un array somma 
+la somma degli elementi di ciascuna colonna della matrice.
+
+(somma[i] = "somma degli elementi dell'i-esima colonna della matrice")
+
+e.g.:   [1 2 3
+
+         4 5 6    -> [12, 15, 18]
+
+         7 8 9]
+
+Notare che la matrice è passata alla funzione tramite un puntatore int* al suo primo elemento ed è quindi 
+come se la matrice fosse appiattita, formando un vettore lungo n-colonne*n-righe.
+
+E' perciò necessario usare l'aritmetica dei puntatori. 
+*/
+
 void leggi_array(int * array, unsigned int dim){
     for(int i = 0; i< dim; i=i+1){
         scanf("%d", array+i);
