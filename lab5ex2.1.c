ce6bd0ee1911a4bf8eaf2c04a69f34d3669f8cdd
Nevia23 <bzaghetto23@gmail.com>
2022-04-11 17:03:31 +0200

    Update lab5ex2.1.c

--- lab5ex2.1.c
+++ lab5ex2.1.c
@@ -9,13 +9,34 @@
 *     printf("%s non palindroma\n",S);
 */
 
+int palindroma ();
 
 int main(void) {
     int len;
     scanf("%d",&len);
     char S[len];
     scanf("%s",S);
-    
+
+    int stringa_palindroma = palindroma (S, len);
+
+    if (stringa_palindroma == 1) {
+        printf("%s palindroma\n",S);
+    } else {
+        printf("%s non palindroma\n",S);
+    }
     
     return 0;
+}
+
+int palindroma (char *X, int dim) {
+    
+    if (dim <= 0) {     
+        return 1;
+    }
+
+    if (*X != *(X + dim - 1)) {
+        return 0;
+    } else {
+        palindroma (X + 1, dim - 2);
+    }   
 }
