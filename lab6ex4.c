164a34a152a64e8cc5ac9370f64c0e4b9e40bc0e
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-04-14 15:52:40 +0200

    Create lab6ex4.c

---
+++ lab6ex4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+* Scrivete la funzione "moltiplicaMatrici" che calcola la moltiplicazione tra
+* due matrici quadrate di numeri interi.
+*
+* L'operazione consiste nell'effettuare il prodotto scalare tra le righe della
+* prima matrice e le colonne della seconda.
+*
+* Esempio:
+* |a b| |e f| ____ |a*e+b*g a*f+b*h|
+* |c d| |g h|      |c*e+d*g c*f+d*h|
+*
+* La funzione riceve come argomenti i puntatori alle due matrici di input ed
+* alla matrice in cui salvare il risultato
+* 
+* void moltiplicaMatrici(int dim, int *mat1, int *mat2, int *matRisultato)
+*
+* Per la consegna, utilizzare:
+* mat1 = [[1,2,3],[4,5,6],[7,8,9]]
+* mat2 = [[9,8,7],[6,5,4],[3,2,1]]
+*
+* Utilizzare il seguente codice per stampare il risultato:
+* for (int i = 0; i < dim; i += 1){
+*   for (int j = 0; j < dim; j += 1){
+*       printf("%d\t", matRisultato[i][j]); 
+*   }
+*   printf("\n");
+* }
+*/
+
+int main (void) {
+
+}
