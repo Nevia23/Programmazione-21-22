a13c285db3117084583399ab19255cca87be3c7b
Nevia23 <bzaghetto23@gmail.com>
2022-04-14 15:49:03 +0200

    Update lab5ex2.3.c

--- lab5ex2.3.c
+++ lab5ex2.3.c
@@ -17,19 +17,55 @@
  * 
  */
 
+int lunghezza (char *A);
+
+char * stringa_lung_max (char stringhe[][256], int num_stringhe);
 
 int main(void) {
 
     int num_stringhe;
+
     scanf("%d", &num_stringhe);
 
     char stringhe[num_stringhe][256];
-    for(int i=0; i<num_stringhe;i+=1) {
+
+    for(int i=0; i < num_stringhe; i += 1) {
         scanf("%255s", stringhe[i]);
     }
 
-    char *s_max = stringa_lung_max(stringhe, num_stringhe);
-    printf("%s\n", (s_max==NULL)?"array vuoto": s_max);
+    char *s_max = stringa_lung_max (stringhe, num_stringhe);
 
+    printf("%s\n", (s_max==NULL)? "array vuoto" : s_max);
+}
 
+char * stringa_lung_max (char stringhe[][256], int dim) {
+
+    if (dim <= 0) {
+        return NULL;
+    }
+
+    int lung_max = lunghezza (stringhe[0]), i, temp, indice_max = 0;
+
+    for (i = 1; i < dim; i += 1) {
+
+        temp = lunghezza (stringhe[i]);
+
+        if (temp > lung_max) {
+            lung_max = temp;
+            indice_max = i;
+        }
+    }
+
+    return stringhe[indice_max];
+}
+
+int lunghezza (char *A) {
+
+    int i;
+
+    for (i = 0; A[i] != '\0'; i += 1) {
+        
+    }
+
+    return i;
 }
