ab530249ef88ac40b7cc6bf23e6725c4f9060663
Nevia23 <bzaghetto23@gmail.com>
2022-04-20 20:15:04 +0200

    Update lab6ex4.c

--- lab6ex4.c
+++ lab6ex4.c
@@ -29,6 +29,40 @@
 * }
 */
 
+void moltiplicaMatrici(int dim, int *mat1, int *mat2, int *matRisultato);
+
 int main (void) {
 
+    int mat1[][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int mat2[][3] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int dim = 3, matRisultato[dim][dim];
+
+moltiplicaMatrici (dim, mat1[0], mat2[0], matRisultato[0]);
+
+for (int i = 0; i < dim; i += 1){
+   for (int j = 0; j < dim; j += 1){
+       printf("%d\t", matRisultato[i][j]); 
+   }
+   printf("\n");
+}
+
+}
+
+void moltiplicaMatrici (int dim, int *mat1, int *mat2, int *matRisultato) {
+
+    int i, j, k, scalare;
+    
+    for (i = 0; i < dim; i += 1) {
+        for (j = 0; j < dim; j += 1) {
+
+            scalare = 0;
+
+            for (k = 0; k < dim; k += 1) {
+                scalare += (*((mat1 + i*dim) + k)) * (*((mat2 + k*dim) + j));
+            }
+
+            *(matRisultato  + i*dim + j) = scalare;
+
+        }
+    }
 }
